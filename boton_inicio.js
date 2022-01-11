e749fe32a72ad725a353ad1db09c6b5cf3a33082
Daniel Martínez <77333826+DaniFortesque@users.noreply.github.com>
2022-01-11 18:37:19 +0100

    Boton de inicio y fondo tapete
    Añadido boton de inicio que muestra las cartas y empieza el juego, tambien se ha añadido un fondo imitando un tapete de cartas.

---
+++ boton_inicio.js
@@ -0,0 +1,7 @@
+//Localizamos el boton y al pusarlo aparece el contenedor
+let boton = document.querySelector("#boton");
+
+boton.addEventListener("click", function() {
+    boton.style.display = "none";
+    document.querySelector("#contenedor").style.display = "flex";
+});
