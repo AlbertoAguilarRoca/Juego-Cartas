fe3235ff77de218a23de8680a7278a7279a44ef4
IsraelDAW <israelantonio.ma44878@cesurformacion.com>
2022-01-12 14:44:56 +0100

    Dar la vuelta a solo 2 cartas
    Ahora permite dar la vuelta a 2 cartas pero aún no las compara, simplemente se queda un momento las 2 cartas y se vuelven a girar ambas.

--- juego.js
+++ juego.js
@@ -1,6 +1,30 @@
+// Para llevar la cuenta de la carta girada
+var girada = false;
+var carta1;
+var carta2;
+
 document.querySelectorAll('.carta').forEach(item => {
-    item.addEventListener("click", function() {
-        girarCarta(item.id);
+    item.addEventListener("click", function() {   
+
+        // Si una carta ha sido girada
+        if (girada) {
+            // Y si no es la misma carta girada
+            if (carta1 != item.id) {
+            carta2 = item.id;
+            girarCarta(carta2);
+            setTimeout(comprobarCartas, 1000);
+            }
+        }
+        // Si no se ha girado la primera carta
+        else {
+            // Guarda en la variable el id
+            carta1 = item.id;
+            // Le da la vuelta a la carta con dicho id
+            girarCarta(carta1);
+            // girada en true para prosegir el juego
+            girada = true;
+        }
+        
     });
 });
 
@@ -10,4 +34,11 @@ function girarCarta(id) {
     }else {
         document.getElementById(id).style.transform = "rotateY(180deg)";
     }
+}
+
+// Ahora mismo en esta funcion solo se le da la vuelta a ambas cartas
+function comprobarCartas() {
+    girarCarta(carta1);
+    girarCarta(carta2);
+    girada = false;
 }
